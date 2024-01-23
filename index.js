d7ffbb49cfa2ab4696af161ad48e6b7bc4553510
riza <146099417+Faturz@users.noreply.github.com>
2024-01-23 22:40:56 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const menuToogle = document.querySelector('.menuToogle input') 
-const nav = document.querySelector('.header ul') 
+const menuToogle = document.querySelector(".menuToogle input");
+const nav = document.querySelector(".header ul");
 
-menuToogle.addEventListener('click' , function () {
-        nav.classList.toggle('slide')
-} )
-
-
-
+menuToogle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
